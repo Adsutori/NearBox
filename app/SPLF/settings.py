e3fdd69626ce58d8a8c1f46434a4d79522808c2e
Adsutori <adsutori@outlook.com>
2026-05-03 21:57:27 +0200

    move secret key to the .env file

--- app/SPLF/settings.py
+++ app/SPLF/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zx0)2grl6b%7jdbxrsaysq-z0#&y51s!5xj1i-q*1r1$cg=l81'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
